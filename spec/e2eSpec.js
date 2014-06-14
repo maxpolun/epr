9e341862d527538a2c5c57a6f994013658c40af2
Max Polun <maxpolun@gmail.com>
2014-06-14 17:39:45 -0400

    initial commit

---
+++ spec/e2eSpec.js
@@ -0,0 +1,14 @@
+'use strict'
+require('jasmine2-pit')
+var testUtil = require('./testUtil')
+
+describe('epr', function(){
+  pit('should add symlinks to node_modules', function(){
+    return testUtil.runInFixture('simple').then(function(){
+      return testUtil.fixtureFilesExist([
+          'node_modules/test1/test1.json',
+          'node_modules/this/is/test/2/test2.js',
+        ])
+    })
+  })
+})
