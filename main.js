a5abafb3161b0dab3d2650695d97da2df9bd8c91
Max Polun <maxpolun@gmail.com>
2014-06-17 17:38:10 -0400

    clean up main

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
 'use strict'
 var readPackage = require('./lib/readPackage'),
     parsePackage = require('./lib/parsePackage'),
-    fs = require('fs'),
-    spawn = require('./lib/spawn'),
-    path = require('path'),
-    _ = require('lodash'),
-    Promise = require('bluebird')
+    epr = require('./lib/epr'),
+    minimist = require('minimist')
 
-var symlink = Promise.promisify(fs.symlink)
 
-function makeLink(src, dest) {
-  var srcPath = path.resolve(src),
-      destPath = path.join(process.cwd(), 'node_modules', dest)
-  return symlink(srcPath, destPath, 'dir')
-}
-
-function ensureNodeModulesExists() {
-  return new Promise(function promiseMkdir(resolve, reject){
-    fs.mkdir(path.join(process.cwd(), 'node_modules'), function resolveMkdir(err){
-      if(err && err.code !== 'EEXIST') {
-        reject(err)
-      }
-      resolve()
-    })
+function entangle() {
+  readPackage().then(function parse(packageStr){
+    return parsePackage(packageStr)
+  }).then(function createSymlinks(linkMapping){
+    epr.entangle(process.cwd(), linkMapping)
   })
 }
 
-ensureNodeModulesExists().then(readPackage).then(function parse(packageStr){
-  return parsePackage(packageStr)
-}).then(function createSymlinks(linkMapping){
-  return Promise.all(_.map(linkMapping, makeLink))
-})
+var commands = {
+  'entangle': entangle
+}
+
+function main(args) {
+  var command = args._[0]
+  if(args._.length === 0) {
+    command = 'entangle'
+  }
+  commands[command]()
+}
+
+main(minimist(process.argv.slice(2)))
